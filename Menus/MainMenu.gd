7248acaf56a6dfc04973ecbaf47b079bccf53798
Dmitry Shabura <shabura.dmitry@gmail.com>
2022-04-21 12:35:28 -0500

    Created Main Menu v2

---
+++ Menus/MainMenu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_TextureButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_StartButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_QuitButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_OptionsButton_pressed():
+	pass # Replace with function body.
