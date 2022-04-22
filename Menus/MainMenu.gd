9922b1c00790e2e22ab3c15bdc7fdac50ccff959
RealmOfReeves <87548979+RealmOfReeves@users.noreply.github.com>
2022-04-21 23:52:23 -0500

    Character And Trinket Classes

--- Menus/MainMenu.gd
+++ Menus/MainMenu.gd
@@ -1,6 +1,11 @@
 extends Control
 
-
+func _ready():
+	var character = Character.new("Bull E. Tshuter", 10,2);
+	var new_trinket = Trinket.new()
+	new_trinket.set_trinket_name("TEST")
+	character.set_trinket(1, new_trinket)
+	print(character.to_string())
 func _on_TextureButton_pressed():
 	pass # Replace with function body.
 
