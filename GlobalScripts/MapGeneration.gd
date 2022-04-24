95de587bec935ba4f71943bdf65ee84ab642aaa2
RealmOfReeves <87548979+RealmOfReeves@users.noreply.github.com>
2022-04-23 19:29:43 -0500

    map generation stuff

--- GlobalScripts/MapGeneration.gd
+++ GlobalScripts/MapGeneration.gd
@@ -1,11 +1,12 @@
-extends Reference
+extends Node
 
 
-func generate_map(var levels,max_nodes_per_level:int)->Array:
+func generate_map(levels:int,max_nodes_per_level:int)->Array:
 	var map = [] #index in array represents level/height in map
 	for i in range(0,levels):
 		var level_nodes = []
 		for n in range(0, max_nodes_per_level):
 			var map_node = MapNode.new();
 			level_nodes.append(map_node)
+		map.append(level_nodes)
 	return map
